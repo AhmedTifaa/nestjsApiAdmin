bff5829ffb015317164744010ec27e85bb6b8343
Affsquare-Dev <ahmedatef@affsquare.com>
2021-10-27 13:38:18 +0200

    fix ssl connection

--- src/app/Services/pages.service.ts
+++ src/app/Services/pages.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class PagesService{
-  private baseUrl='http://onmarket-nest.herokuapp.com';
+  private baseUrl='https://onmarket-nest.herokuapp.com';
  constructor(private http:HttpClient){}
   listOrders(){
     return this.http.get(`${this.baseUrl}/order`);
